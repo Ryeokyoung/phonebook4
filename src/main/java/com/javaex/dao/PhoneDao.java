0603b2bfdfcff16e58a47e67af2aed51e7a94b4e
Ryeokyoung <ryeokyoungk@gmail.com>
2022-06-17 14:28:30 +0900

    -

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -14,47 +14,67 @@ public class PhoneDao {
 	@Autowired
 	private SqlSession sqlSession;
 
-	
-	public int personInsert(PersonVo pVo) {
-		// System.out.println(pVo);
-		int count = sqlSession.insert("phonebook.personInsert", pVo);
+	// 전화번호 전체가져오기
+	public List<PersonVo> getPersonList() {
+		System.out.println("PhoneDao>getPersonList()");
+
+		List<PersonVo> personList = sqlSession.selectList("phonebook.selectList");
+		System.out.println(personList);
+		return personList;
+	}
+
+	// 추가
+	public int personInsert(PersonVo personVo) {
+
+		int count = sqlSession.insert("phonebook.personInsert", personVo);
 		return count;
 	}
 
-	public int personUpdate(PersonVo pVo) {
-		//System.out.println(pVo);
-		int count = sqlSession.update("phonebook.personUpdate", pVo);
-		package com.javaex.dao;
+	// 사람추가(map을 사용하는 가상의 상황: 문법설명)
+	public int personInsert2(Map<String, String> pMap) {
+		System.out.println("PhoneDao>personInsert2()");
 
-import com.javaex.vo.*;
-import java.util.*;
+		// System.out.println(pMap.toString());
 
-public class TestApp {
-	public static void main(String[] args) {
-		PhoneDao pDao = new PhoneDao();
-		
-		List<PersonVo> pList = pDao.personSelect();
-		
-		System.out.println(pList);
-	}
-}return count;
+		int count = sqlSession.insert("phonebook.personInsert2", pMap);
+
+		return 1;
 	}
 
-	public int personDelete(int personId) {
-		// System.out.println(personId);
-		int count = sqlSession.delete("phonebook.personDelete", personId);
+	// 삭제
+	public int personDelete(int no) {
+
+		int count = sqlSession.delete("phonebook.personDelete", no);
 		return count;
 	}
 
-	public List<PersonVo> personSelect() {
-		List<PersonVo> pList = sqlSession.selectList("phonebook.personSelect");
-		// System.out.println(pList);
-		return pList;
+	// 1명 정보 가져오기
+	public PersonVo getPerson(int no) {
+		System.out.println("PhoneDao>getPerson()");
+
+		PersonVo personVo = sqlSession.selectOne("phonebook.getPerson", no);
+		System.out.println(personVo);
+
+		return personVo;
+
 	}
 
-	public PersonVo getPerson(int personId) {
-		PersonVo pVo = sqlSession.selectOne("phonebook.getPerson", personId);
-		// System.out.println(pVo);
-		return pVo;
+	// 1명 정보 가져오기 map을 사용하는 가상의 상황: 문법설명
+	public Map<String, Object> getPerson2(int no) {
+		System.out.println("PhoneDao>getPerson2()");
+		System.out.println(no);
+
+		Map<String, Object> pMap = sqlSession.selectOne("phonebook.getPerson2", no);
+
+		return pMap;
+	}
+
+	// 사람 수정
+	public int personUpdate(PersonVo personVo) {
+		System.out.println("PhoneDao>personUpdate()");
+
+		int count = sqlSession.update("phonebook.personUpdate", personVo);
+
+		return count;
 	}
 }
